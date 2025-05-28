bf2b6af6a8227088f4e219f309bd68e44692aecf
Marcel <123411996+MarcelStuebl@users.noreply.github.com>
2025-05-28 18:36:18 +0200

    fix pictures

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,5 @@ dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("com.google.android.material:material:1.9.0")
-    implementation("com.github.chrisbanes:PhotoView:2.3.0")
 
 }
